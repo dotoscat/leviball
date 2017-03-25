b91c6338c660e056f0a545085f1442007ceb291d
Oscar Triano dotoscat <user@example.com>
2017-03-25 11:24:42 +0100

    Hello world!

---
+++ main.py
@@ -0,0 +1,18 @@
+import pygame
+
+def main():
+    import pyglet
+    window = pyglet.window.Window()
+    label = pyglet.text.Label('Hello world!',
+                              font_name='Verdana',
+                              font_size=24,
+                              x=window.width//2, y=window.height//2,
+                              anchor_x='center', anchor_y='center')
+    @window.event
+    def on_draw():
+        window.clear()
+        label.draw()
+    pyglet.app.run()
+
+if __name__ == '__main__':
+    main()
