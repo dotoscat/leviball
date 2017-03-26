0fc9b13eece9c16ab34c2b461f2a7ca006069c04
Oscar Triano dotoscat <user@example.com>
2017-03-26 19:15:30 +0200

    Add GameData class

---
+++ game_data.py
@@ -0,0 +1,37 @@
+class GameData:
+    GAME_OVER = 0
+    RUNNING = 1
+    PAUSED = 2
+    def __init__(self):
+        self.reset()
+
+    def update(self, dt):
+        self.secs += dt*self.speed
+        if self.secs > 1.0:
+            self.secs = 0.0
+            self.speed += 0.1
+            self.meters += 1.
+
+    def is_running(self):
+        return self.status == GameData.RUNNING
+
+    def is_paused(self):
+        return self.status == GameData.PAUSED
+
+    def is_over(self):
+        return self.status == GameData.GAME_OVER
+
+    def set_running(self):
+        self.status = GameData.RUNNING
+
+    def set_paused(self):
+        self.status = GameData.PAUSED
+
+    def set_over(self):
+        self.status = GameData.GAME_OVER
+
+    def reset(self):
+        self.speed = 1.
+        self.meters = 0
+        self.secs = 0.
+        self.set_over()
