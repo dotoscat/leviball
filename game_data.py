5de6f83d74a8b6f38132d116cc7ee0a17ac26e7c
Oscar Triano dotoscat <user@example.com>
2017-04-05 17:32:23 +0200

    Add GPL3 license to code

--- game_data.py
+++ game_data.py
@@ -1,3 +1,19 @@
+##game_data.py: Manages game status
+##Copyright (C) 2017  Oscar Triano 'dotoscat'
+##
+##This program is free software: you can redistribute it and/or modify
+##it under the terms of the GNU General Public License as published by
+##the Free Software Foundation, either version 3 of the License, or
+##(at your option) any later version.
+##
+##This program is distributed in the hope that it will be useful,
+##but WITHOUT ANY WARRANTY; without even the implied warranty of
+##MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+##GNU General Public License for more details.
+##
+##You should have received a copy of the GNU General Public License
+##along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 class GameData(object):
     GAME_OVER = 0
     RUNNING = 1
