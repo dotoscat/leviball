b63c42c13f875272af9224de212c9c6c1c38571f
Oscar Triano dotoscat <user@example.com>
2017-04-05 17:33:03 +0200

    Import version from main module

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ from cx_Freeze import setup, Executable
 buildOptions = dict(packages = ['pyglet'], excludes = [])
 
 import sys
+from main import VERSION
 base = 'Win32GUI' if sys.platform=='win32' else None
 
 executables = [
@@ -12,7 +13,7 @@ executables = [
 ]
 
 setup(name='Leviball',
-      version = '1.0a',
+      version = VERSION,
       description = 'Avoid those little things in your way',
       options = dict(build_exe = buildOptions),
       executables = executables)
